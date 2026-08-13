152635bd7f0c099671e2eb779ca0301d86d3aa33
Dharaheshh V <99342421+Dharaheshh@users.noreply.github.com>
2026-08-13 09:04:12 +0530

    Time: 1 ms (100%), Space: 44 MB (35.12%) - LeetHub

---
+++ 0065-valid-number/0065-valid-number.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isNumber(String s) {
+        boolean digit = false;
+        boolean e = false;
+        boolean dot = false;
+        for(int i=0;i<s.length();i++){
+            char c = s.charAt(i);
+            if(Character.isDigit(c)) digit = true;
+            else if(c=='+'||c=='-'){
+                if(i!=0 && s.charAt(i-1)!='e'&&s.charAt(i-1)!='E') return false;
+            }
+            else if(c=='.'){
+                if(dot || e) return false;
+                dot = true;
+            }
+            else if(c=='e'||c=='E'){
+                if(e||!digit) return false;
+                e = true;
+                digit = false;
+            }
+            else return false;
+        }
+        return digit;
+
+    }
+}
