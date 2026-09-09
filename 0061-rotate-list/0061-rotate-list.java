ba2ed209855b2d9a18c984f5c44463577acf5eb8
Dharaheshh V <99342421+Dharaheshh@users.noreply.github.com>
2026-09-09 21:46:27 +0530

    Time: 0 ms (100%), Space: 44.6 MB (10.05%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.java
+++ 0061-rotate-list/0061-rotate-list.java
@@ -8,23 +8,49 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// class Solution {
+//     public ListNode rotateRight(ListNode head, int k) {
+//         if(k==0 || head==null || head.next==null) return head;
+//         int n = 1;
+//         ListNode t = head;
+//         while (t.next != null) {
+//             t = t.next;
+//             n++;
+//         }
+//         k%=n;
+//         if(k==0) return head;
+//        // return head;
+//         t.next = head;
+//         ListNode end = head;
+//         for(int i=1;i<n-k;i++) end = end.next;
+//         ListNode start = end.next;
+//         end.next = null;
+//         return start;
+//     }
+// }
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-        if(k==0 || head==null || head.next==null) return head;
+        if (head == null || head.next == null || k == 0)return head;
         int n = 1;
-        ListNode t = head;
-        while (t.next != null) {
-            t = t.next;
+        ListNode tail = head;
+        while (tail.next != null) {
+            tail = tail.next;
             n++;
         }
-        k%=n;
-        if(k==0) return head;
-       // return head;
-        t.next = head;
-        ListNode end = head;
-        for(int i=1;i<n-k;i++) end = end.next;
-        ListNode start = end.next;
-        end.next = null;
-        return start;
+        k = k % n;
+        if (k == 0)return head;
+        tail.next = head;
+        ListNode fast = head;
+        ListNode slow = head;
+        for (int i = 0; i < k; i++) {
+            fast = fast.next;
+        }
+        while (fast.next != head) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        ListNode newHead = slow.next;
+        slow.next = null;
+        return newHead;
     }
 }
